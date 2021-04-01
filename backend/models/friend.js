07ba8ce763f893c7c8e28c361c6e9bf29bd0c4df
Suhan Park <vixx170627@gmail.com>
2021-04-01 19:13:15 +0900

    회원가입 완성

--- backend/models/friend.js
+++ backend/models/friend.js
@@ -2,13 +2,6 @@ const { sequelize, Sequelize, User } = require(".");
 
 module.exports = (sequelize,DataTypes)=>{
     return sequelize.define('friend',{
-        userId:{
-            type:DataTypes.UUID,
-            references:{
-                model:'users',
-                key:'userId'
-            }
-        },
         friendId:{
             type:DataTypes.UUID,
             references:{
