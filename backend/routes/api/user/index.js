c6868ac16325aa2f06146143c2fde34aa5157837
Suhan Park <vixx170627@gmail.com>
2021-04-04 09:54:13 +0900

    jwt를 통한 로그인 구현

--- backend/routes/api/user/index.js
+++ backend/routes/api/user/index.js
@@ -3,6 +3,7 @@ const controller = require('./controller')
 
 router.post('/signup',controller.register)
 router.post('/get-user',controller.getUser)
+router.post('/login',controller.login)
 router.post('/edit-group',controller.makeGroup)
 router.post('/make-member',controller.makeMember)
 router.post('/edit-friend',controller.makeFriend)
