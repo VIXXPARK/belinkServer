27d6a4799345b53deff5d210bcce3255597bc0e3
k-ms1998 <71029015+k-ms1998@users.noreply.github.com>
2021-04-14 21:20:22 +0900

    test_stores 수정
    방탈출 카페, 만화 카페, 보드게임 카페 등 '테마 카페'들이 누락되거나 그냥 일반 카페(카테고리 코드: CE7)으로 저장되서 수정

--- test_data/routes/test_stores.js
+++ test_data/routes/test_stores.js
@@ -6,32 +6,56 @@ const promisify = require('util').promisify;
 const async = require('async');
 
 const location = ["건대입구역", "잠실역", "강남역", "왕십리역", "홍대입구역", "여의도역"];
-const plCode = ["FD6", "CE7", "HP8", "AD5", "PC"];
+const plCode = ["FD6", "CE7", "PC", "THM"];
 
 var getStores = async function(){
     for(const curLocation of location){
         for(const curCode of plCode){
             if(curCode == "PC"){
-            await requestApi({
-                uri: encodeURI("https://dapi.kakao.com/v2/local/search/keyword.json?page=1&size=15&sort=accuracy&query="+(curLocation+" PC방")),
-                method: "GET",
-                headers:{
-                    'Authorization': 'KakaoAK c9f67cf11819f1c1e3c318b99d7dfac1'
-                },
-                encoding:'utf-8'
-                } , async function(error, res, body){
-                var parsedBody = JSON.parse(body);
-                var places = parsedBody["documents"];
-                        
-                for(const curPlace of places){
-                    await models.Store.create({
-                        id: curPlace["id"],
-                        storeName: curPlace["place_name"],
-                        storeLocation: curPlace["road_address_name"],
-                        storeType: curPlace["category_group_code"]
-                    });
-                }
-            })
+                await requestApi({
+                    uri: encodeURI("https://dapi.kakao.com/v2/local/search/keyword.json?page=1&size=15&sort=accuracy&query="+(curLocation+" PC방")),
+                    method: "GET",
+                    headers:{
+                        'Authorization': 'KakaoAK c9f67cf11819f1c1e3c318b99d7dfac1'
+                    },
+                    encoding:'utf-8'
+                    } , async function(error, res, body){
+                    var parsedBody = JSON.parse(body);
+                    var places = parsedBody["documents"];
+                            
+                    for(const curPlace of places){
+                        await models.Store.create({
+                            id: curPlace["id"],
+                            storeName: curPlace["place_name"],
+                            storeLocation: curPlace["road_address_name"],
+                            storeType: "PC"
+                        });
+                    }
+                    
+                })
+            }
+            else if(curCode == "THM"){
+                await requestApi({
+                    uri: encodeURI("https://dapi.kakao.com/v2/local/search/keyword.json?page=1&size=15&sort=accuracy&query="+(curLocation+" 테마카페")),
+                    method: "GET",
+                    headers:{
+                        'Authorization': 'KakaoAK c9f67cf11819f1c1e3c318b99d7dfac1'
+                    },
+                    encoding:'utf-8'
+                    } , async function(error, res, body){
+                    var parsedBody = JSON.parse(body);
+                    var places = parsedBody["documents"];
+                            
+                    for(const curPlace of places){
+                        await models.Store.create({
+                            id: curPlace["id"],
+                            storeName: curPlace["place_name"],
+                            storeLocation: curPlace["road_address_name"],
+                            storeType: "THM"
+                        });
+                    }
+                    
+                })
             }
             else{
                 await requestApi({
