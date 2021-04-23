55d934066f480d26134bbe685528500f58c48255
Suhan Park <vixx170627@gmail.com>
2021-04-23 19:37:24 +0900

    test password to null

--- models/index.js
+++ models/index.js
@@ -5,6 +5,9 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(path.join(__dirname, '..', 'config', 'config.js'))[env];
 const db = {};
 
+if(env=='test'){
+    config.password=null    
+}
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
