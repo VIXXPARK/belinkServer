d333d5e6e874d4ce74d18d36dbcff563ed8db0a4
Suhan Park <vixx170627@gmail.com>
2021-05-17 18:50:02 +0900

    index 수정

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,12 @@ const router = require('express').Router()
 const user =require('./user')
 const location = require('./location');
 const push = require('./push');
+const store= require('./store');
+const sms= require('./sms');
 
 router.use('/user', user)
 router.use('/location', location)
 router.use('/push', push)
-
+router.use('/store',store)
+router.use('/sms',sms);
 module.exports = router
