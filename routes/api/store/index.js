6054900f97de55dbedb1829e0777390931f64bde
Suhan Park <vixx170627@gmail.com>
2021-05-17 18:57:19 +0900

    npm install request

--- routes/api/store/index.js
+++ routes/api/store/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const controller = require('./controller')
 
-console.log( __dirname );
 router.post('/signup',controller.signup);       
 
 module.exports = router
