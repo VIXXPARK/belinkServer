1f267c6bd3ae950f396e4f35d5180a981f4eb892
gesture02 <gesture02@gmail.com>
2021-05-24 15:09:35 +0900

    viewQuery 수정

--- models/useableVisit.js
+++ models/useableVisit.js
@@ -1,5 +1,5 @@
 const view_name = 'useableVisits';
-const viewQuery = "CREATE VIEW IF NOT EXISTS useableVisits AS SELECT v.id as myId, DAYOFWEEK(v.createdAt) as myDay, HOUR(v.createdAt) as myHour, IFNULL((SELECT s2.storeType FROM visits as v2 LEFT JOIN stores as s2 ON v2.storeId = s2.id WHERE TIMEDIFF((SELECT createdAt FROM visits WHERE id = myId), v2.createdAt) BETWEEN '00:00:01' AND '3:00:00' AND v2.userId = (SELECT userId FROM visits WHERE id = myId) ORDER BY v2.createdAt desc LIMIT 1),'none') AS prior, s.storeType FROM visits as v LEFT JOIN stores as s ON v.storeId = s.id WHERE v.storeId = s.id";
+const viewQuery = "CREATE OR REPLACE VIEW useableVisits AS SELECT v.id as myId, DAYOFWEEK(v.createdAt) as myDay, HOUR(v.createdAt) as myHour, IFNULL((SELECT s2.storeType FROM visits as v2 LEFT JOIN stores as s2 ON v2.storeId = s2.id WHERE TIMEDIFF((SELECT createdAt FROM visits WHERE id = myId), v2.createdAt) BETWEEN '00:00:01' AND '3:00:00' AND v2.userId = (SELECT userId FROM visits WHERE id = myId) ORDER BY v2.createdAt desc LIMIT 1),'none') AS prior, s.storeType FROM visits as v LEFT JOIN stores as s ON v.storeId = s.id WHERE v.storeId = s.id";
 
 module.exports = (sequelize, DataTypes) => {
     sequelize.query(viewQuery);
