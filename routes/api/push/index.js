5f54a293d9056097a167fa74fa56021a3d073c2d
Suhan Park <vixx170627@gmail.com>
2021-05-25 14:00:50 +0900

    get to post

--- routes/api/push/index.js
+++ routes/api/push/index.js
@@ -3,7 +3,7 @@ const controller = require('./controller')
 
 router.post('/nfcPushMsg', controller.nfcPushMsg)
 router.post('/accepted', controller.accept)
-router.get('/rejected', controller.reject)
+router.post('/rejected', controller.reject)
 router.post('/infectionPush', controller.groupInfectionPush)
 router.post('/test', controller.test)
 module.exports = router
