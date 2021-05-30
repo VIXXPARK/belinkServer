6e2c87e1747769e1a587f119c7fa181efb1c7de9
Suhan Park <vixx170627@gmail.com>
2021-05-30 17:26:11 +0900

    주석 정리

--- routes/api/push/controller.js
+++ routes/api/push/controller.js
@@ -10,13 +10,10 @@ exports.nfcPushMsg = async (req, res, next) => {
             {
                 userId: userId,
                 cnt: 0,
-                //accpeted: true
             },
             { where: { teamId : team_room }})
 
         const noti = {
-            // title: '그룹 nfc',
-            // body: '그룹 수락 요청입니다.'
         }
         const data = {
             title: '그룹 nfc',
@@ -48,8 +45,7 @@ exports.accept = async (req, res, next) => {
         const cur = await model.Accept.findAll({ attributes: ['cnt', 'total'], where: { teamId: team_room }})
         
         const noti = {
-            // title: '완료되었습니다.',
-            // body: '방문기록 작성이 완료되었습니다.'
+
         }
         const data = {
             title: '완료되었습니다.',
@@ -62,8 +58,7 @@ exports.accept = async (req, res, next) => {
         console.log(cur)
         if(cur[0].total == cur[0].cnt)
         {
-            //pushService.groupPush(req, res, noti, data);
-            //pushService.storePush(req, res, cur[0].total);
+
             console.log("if문 안에 들어옴")
             console.log("acceptPush문으로 들어간다.")
             pushService.acceptPush(req, res, noti, data, cur[0].total);
@@ -86,13 +81,9 @@ exports.reject = async (req, res) => {
     const { team_room } = req.body;
 
     try{
-        // await model.Accept.update(
-        //     { accepted: false },
-        //     { where: { teamId : team_room }}
-        // )
+
         const noti = {
-            // title: '취소되었습니다.',
-            // body: '거절되었습니다.'
+
         }
         const data = {
             title: '취소되었습니다.',
@@ -147,50 +138,7 @@ exports.test = async(req, res) => {
             success : true,
             message : jsonArray
         })
-        // const result = await model.Member.findAll({
-        //     raw:true,
-        //     where: { team_room: team_room },
-        //     include: [{ model: model.User, as: 'teamMember', attributes: ['token'] }]
-        //     // required: true = inner join
-        //     // right: true = right outer join
-        // })
-        // //console.log(result);
-        // const array = []
-        
-        // for(const cur of result){
-        //     array.push(cur['teamMember.token']);
-        // }
-        // console.log(array);
-        // res.json({
-        //     success: true,
-        //     message: array
-        // })
-        // const team_rooms = await model.Member.findAll({
-        //     attributes: ['team_room'],
-        //     where: { team_member: userId }
-        // })
-        // const teamRoomArray = [];
-        // team_rooms.forEach((item, idx)=>{
-        //     teamRoomArray.push(item.team_room);
-        // });
 
-        // console.log(teamRoomArray);
-        // const result = await model.Member.findAll({
-        //     attributes: [],
-        //     where: { team_room: teamRoomArray },
-        //     include: [{ model: model.User, as: 'teamMember', attributes: ['id','username', 'token'] }]
-        // })
-        // const infectArray = [];
-        // result.forEach((item, idx)=>{
-        //     if(item.teamMember.id != userId)
-        //         infectArray.push(item.teamMember.username);
-        // });
-        // console.log(infectArray);
-
-        // res.json({
-        //     success: true,
-        //     message: result
-        // })
         
     } catch (err) {
         res.status(404).json({
