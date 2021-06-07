ce99f133918399e1862f0a6d785f82ea02272f13
Suhan Park <vixx170627@gmail.com>
2021-06-07 18:35:23 +0900

    코드 정렬

--- models/member.js
+++ models/member.js
@@ -1,4 +1,3 @@
-module.exports = (sequelize,DataTypes)=>{
-    return sequelize.define("member",{
-    })
-}
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("member", {});
+};
--- models/user.js
+++ models/user.js
@@ -1,37 +1,40 @@
 const { Sequelize } = require(".");
 
-module.exports = (sequelize,DataTypes)=>{
-    return sequelize.define('user',{
-        id:{
-            type:DataTypes.UUID,
-            allowNull:false,
-            primaryKey: true,
-            defaultValue:DataTypes.UUIDV4
-        },
-        phNum:{
-            type:DataTypes.STRING,
-        },
-        username:{
-            type:DataTypes.STRING,
-            allowNull:false
-        },
-        active:{
-            type:DataTypes.BOOLEAN,
-            defaultValue: true
-        },
-        admin:{
-            type:DataTypes.BOOLEAN,
-            defaultValue:false
-        },
-        infect:{
-            type:DataTypes.TINYINT,
-            defaultValue:0
-        },
-        token:{
-            type:DataTypes.STRING,
-        }
-
-    },{
-        timestamps:true,
-    });
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "user",
+    {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      phNum: {
+        type: DataTypes.STRING,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+      admin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      infect: {
+        type: DataTypes.TINYINT,
+        defaultValue: 0,
+      },
+      token: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
 };
